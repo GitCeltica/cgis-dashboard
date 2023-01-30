7b34236bd8b28d1fca592fb57a50fb1e7416c6b9
Jonatas Miguel <daniel.lucas@celtica.com.br>
2023-01-30 16:27:23 -0300

    ajuste labels os
    as labels agora estão limitadas para duas casas decimais

--- frontend/cDashboard/src/components/LabelOS/labelOS.js
+++ frontend/cDashboard/src/components/LabelOS/labelOS.js
@@ -8,7 +8,9 @@ export default function LabelOS (dados){
             <label>{data.name}</label>
             <div>
                 <span className="valor">
-                    {data.name === "Custo Medio" ? ((data.data).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})) : ((data.data).toLocaleString('pt-br'))                }
+                    {data.name === "Custo Medio" ? ((data.data).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})) : (parseFloat((data.data).toFixed(2)).toLocaleString('pt-br')) }
+                    {/* {data.name === "Custo Medio" ? ((data.data).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})) : ((data.data).toLocaleString('pt-br')) } */}
+
                 </span>
                 {data.name === "Tempo médio correções OS de vazamento" || data.name === "Tempo Medio" || data.name === "Distancia Media"  ? (
                    data.name === "Distancia Media" ? (<span className="medida">km</span>) : (<span className="medida">min</span>) 
